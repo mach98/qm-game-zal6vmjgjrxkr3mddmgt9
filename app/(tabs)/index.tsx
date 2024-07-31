5d65ca7bfc63cbdfd05bcce77beba2eae01784c0
Williams Bolade <dbolade2000@gmail.com>
2024-07-31 12:53:20 +0100

    tsconfig modified

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
-import { SafeAreaView, View, Text, ImageBackground } from 'react-native';
+import { SafeAreaView, View, Text, ImageBackground, Image } from 'react-native';
 import React from 'react';
+import { BackgroundImage } from '@/constants/images';
 
 const Home = () => {
   return (
     <SafeAreaView className='flex-1 bg-gray mt-16'>
       <View>
+        <Image
+          style={{ width: 200, height: 20 }}
+          source={BackgroundImage}
+          resizeMode='contain'
+        />
         <Text>Home</Text>
       </View>
     </SafeAreaView>
